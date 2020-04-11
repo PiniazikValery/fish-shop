022c6131e5c064802b73876229947f312be94569
valerypiniazik <>
2020-04-11 17:13:58 +0300

    added server with basic auth

---
+++ server/passport_auth/strategies/bearer/index.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const BearerStrategy = require('passport-http-bearer').Strategy;
+const config = require('config');
+const jwt = require('jwt-simple');
+const User = require('../../../models/account_models/user');
+
+passport.use(new BearerStrategy((token, done) => {
+    try {
+        const { user_id } = jwt.decode(token, config.get('JWT.secret'));
+        User.findById(user_id).then(user => {
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        });
+    } catch (error) {
+        done(null, false);
+    }
+}));
+
+module.exports = passport;

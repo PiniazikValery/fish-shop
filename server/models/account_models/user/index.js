bb61ac27319e5eb54a6165e2a3a119baeabbfe37
valerypiniazik <>
2020-04-11 19:35:50 +0300

    make all files pretty

--- server/models/account_models/user/index.js
+++ server/models/account_models/user/index.js
@@ -1,48 +1,48 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
-    role: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Role'
-    },
-    updated_at: { type: Date, default: Date.now },
+  username: String,
+  email: String,
+  password: String,
+  role: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Role",
+  },
+  updated_at: {type: Date, default: Date.now},
 });
 
-UserSchema.pre('save', function hashPassword(next) {
-    if (this.password) {
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(this.password, salt, (err, hash) => {
-                if (err) {
-                    return next(err);
-                }
-                this.password = hash;
-                return next();
-            });
-        });
-    } else {
+UserSchema.pre("save", function hashPassword(next) {
+  if (this.password) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(this.password, salt, (err, hash) => {
+        if (err) {
+          return next(err);
+        }
+        this.password = hash;
         return next();
-    }
+      });
+    });
+  } else {
+    return next();
+  }
 });
 
 UserSchema.statics.getCount = () => {
-    return User.countDocuments({}, (err, count) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return count;
-        }
-    });
+  return User.countDocuments({}, (err, count) => {
+    if (err) {
+      console.log(err);
+    } else {
+      return count;
+    }
+  });
 };
 
-UserSchema.statics.findUserByEmail = (email) => {
-    return User.findOne({ email: email }).then(user => {
-        return Promise.resolve(user);
-    });
+UserSchema.statics.findUserByEmail = email => {
+  return User.findOne({email: email}).then(user => {
+    return Promise.resolve(user);
+  });
 };
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 module.exports = User;

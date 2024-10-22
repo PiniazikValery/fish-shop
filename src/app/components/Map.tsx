3a3c8736f28258a8d873f2fd254ee9be6aaaf1a2
Valery Piniazik <val.piniazik@gmail.com>
2024-10-22 23:10:53 +0300

    feat: added pin for map

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,16 +1,34 @@
 "use client";
 
-import { useEffect } from "react";
-import type { DomEventHandler, YMapLocationRequest } from "ymaps3";
+import { useCallback, useEffect, useRef } from "react";
+import {
+  YMapEntity,
+  type DomEvent,
+  type DomEventHandler,
+  type DomEventHandlerObject,
+  type LngLat,
+  type YMap,
+  type YMapLocationRequest,
+} from "ymaps3";
 
 import { useMap } from "@/app/providers/map-provider";
 
-async function injectMap(onClick?: DomEventHandler) {
+interface MapProps {
+  onLocationChange?: (lnglat: LngLat) => void;
+}
+
+function loadMap(onClick?: DomEventHandler) {
   const LOCATION: YMapLocationRequest = {
     center: [30.334574119049794, 53.88162941583986],
     zoom: 11,
   };
-  const { YMap, YMapDefaultSchemeLayer, YMapListener } = ymaps3;
+  const {
+    YMap,
+    YMapDefaultSchemeLayer,
+    YMapListener,
+    YMapFeatureDataSource,
+    YMapDefaultFeaturesLayer,
+  } = ymaps3;
   const mapListener = new YMapListener({
     layer: "any",
     onClick: onClick,
@@ -21,26 +39,63 @@ async function injectMap(onClick?: DomEventHandler) {
       location: LOCATION,
     });
     map.addChild(new YMapDefaultSchemeLayer({}));
+    map.addChild(new YMapDefaultFeaturesLayer({}));
+    map.addChild(
+      new YMapFeatureDataSource({
+        id: "featureSource",
+      })
+    );
     map.addChild(mapListener);
+    return map;
   }
+
+  return null;
 }
 
-export default function Map() {
-  const { mapLoaded } = useMap();
+export default function Map({ onLocationChange }: MapProps) {
+  const { mapScriptLoaded } = useMap();
+  const map = useRef<YMap | null>(null);
+  const previusMarker = useRef<YMapEntity<unknown> | null>(null);
+  // const [selectedLocation, setSelectedLocation] = useState<LngLat | null>(null);
+  const rerenderMarker = (coordinates: LngLat) => {
+    const markerElement = document.createElement("div");
+    markerElement.className =
+      "flex flex-col items-center justify-center transform -translate-y-[100%] -translate-x-[50%]";
+
+    // Create the red circle (head of the pin)
+    const markerHead = document.createElement("div");
+    markerHead.className = "bg-red-500 rounded-full w-4 h-4 top-0 transform";
+
+    // Create the black line (pin)
+    const markerLine = document.createElement("div");
+    markerLine.className = "bg-black w-0.5 h-3 transform";
+
+    // Append the head and line to the marker element
+    markerElement.appendChild(markerHead);
+    markerElement.appendChild(markerLine);
+    const marker = new ymaps3.YMapMarker(
+      {
+        coordinates,
+      },
+      markerElement
+    );
+
+    if (previusMarker.current) map.current!.removeChild(previusMarker.current);
+
+    map.current?.addChild(marker);
+    previusMarker.current = marker;
+  };
+  const onMapClick = useCallback(
+    (_layer: DomEventHandlerObject, { coordinates }: DomEvent) => {
+      onLocationChange?.(coordinates);
+      rerenderMarker(coordinates);
+    },
+    [onLocationChange]
+  );
   useEffect(() => {
-    if (mapLoaded) {
-      injectMap((layer, coordinates) => {
-        console.log("layer: ", layer);
-        console.log("coordinates: ", coordinates);
-        // console.log("object: ", object);
-        console.log("User clicked on map");
-        ymaps3
-          .search({ text: coordinates.coordinates.toString() })
-          .then((test) => {
-            console.log("test: ", test);
-          });
-      });
+    if (mapScriptLoaded) {
+      map.current = loadMap(onMapClick);
     }
-  }, [mapLoaded]);
+  }, [mapScriptLoaded, onMapClick]);
   return <div id="map" style={{ width: 700, height: 700 }}></div>;
 }
--- src/app/providers/map-provider.tsx
+++ src/app/providers/map-provider.tsx
@@ -1,19 +1,46 @@
 "use client";
 
+import { LngLat } from "@yandex/ymaps3-types";
 import Script from "next/script";
-import { createContext, useContext, useState } from "react";
+import { createContext, useCallback, useContext, useState } from "react";
 
-export const MapContext = createContext({
-  mapLoaded: false,
+export const MapContext = createContext<{
+  mapScriptLoaded: boolean;
+  coordsToAddress: (lnglat: LngLat) => Promise<string>;
+}>({
+  mapScriptLoaded: false,
+  coordsToAddress: () => {
+    throw new Error("Not initialized");
+  },
 });
 
 export const MapProvider: React.FC<{
   children?: React.ReactNode;
 }> = (props) => {
-  const [mapLoaded, setMapLoaded] = useState(false);
+  const [mapScriptLoaded, setMapLoaded] = useState(false);
+
+  const coordsToAddress = useCallback(
+    async (lnglat: LngLat) => {
+      if (mapScriptLoaded) {
+        const address = await ymaps3.search({
+          text: lnglat.toString(),
+        });
+        const { description, name } = address[0].properties;
+        return `${description}, ${name}`;
+      } else {
+        throw new Error("Map not initialized");
+      }
+    },
+    [mapScriptLoaded]
+  );
 
   return (
-    <MapContext.Provider value={{ mapLoaded }}>
+    <MapContext.Provider
+      value={{
+        mapScriptLoaded,
+        coordsToAddress,
+      }}
+    >
       <Script
         src={`https://api-maps.yandex.ru/v3/?apikey=${process.env.NEXT_PUBLIC_YA_MAPS_KEY}&lang=en_US`}
         strategy="afterInteractive"

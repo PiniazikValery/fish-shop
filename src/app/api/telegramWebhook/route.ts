283418a2f1b479b32a6a3d73409d01625405e3b0
PiniazikValery <36072114+PiniazikValery@users.noreply.github.com>
2024-11-09 15:54:42 +0200

    Update route.ts

--- src/app/api/telegramWebhook/route.ts
+++ src/app/api/telegramWebhook/route.ts
@@ -1,11 +1,28 @@
-import { getBot } from "@/app/lib/telegram";
-import { NextResponse } from "next/server";
+// import { getBot } from "@/app/lib/telegram";
+// import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  const bot = await getBot();
-  const body = await req.json();
+// export async function POST(req: Request) {
+//   const bot = await getBot();
+//   const body = await req.json();
 
-  await bot.handleUpdate(body);
+//   await bot.handleUpdate(body);
 
-  return NextResponse.json({ status: "Message processed" });
-}
+//   return NextResponse.json({ status: "Message processed" });
+// }
+export const dynamic = 'force-dynamic'
+
+export const fetchCache = 'force-no-store'
+
+import { Bot, webhookCallback } from 'grammy'
+
+const TELEGRAM_TOKEN = process.env.NEXT_TELEGRAM_TOKEN as string;
+
+if (!TELEGRAM_TOKEN) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.')
+
+const bot = new Bot(TELEGRAM_TOKEN)
+ bot.command("start", async (ctx) => {
+    console.log("bot start");
+  });
+
+export const POST = webhookCallback(bot, 'std/http')
+

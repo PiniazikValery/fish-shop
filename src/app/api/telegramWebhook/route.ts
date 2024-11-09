71291d9c5aef57615383d350d74926cd9b588371
PiniazikValery <36072114+PiniazikValery@users.noreply.github.com>
2024-11-09 15:58:38 +0200

    Update route.ts

--- src/app/api/telegramWebhook/route.ts
+++ src/app/api/telegramWebhook/route.ts
@@ -14,14 +14,32 @@ export const dynamic = 'force-dynamic'
 export const fetchCache = 'force-no-store'
 
 import { Bot, webhookCallback } from 'grammy'
+import { getDb } from "@/db";
+import { ChatId } from "@/db/entity/ChatId";
 
 const TELEGRAM_TOKEN = process.env.NEXT_TELEGRAM_TOKEN as string;
 
 if (!TELEGRAM_TOKEN) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.')
 
 const bot = new Bot(TELEGRAM_TOKEN)
- bot.command("start", async () => {
+ bot.command("start", async (ctx) => {
     console.log("bot start");
+    const chatId = ctx.chat.id;
+
+    const db = await getDb();
+    const chatIdRepository = db.getRepository(ChatId);
+    try {
+      // Check if chatId already exists
+      const existingChatId = await chatIdRepository.findOne({
+        where: { chatId },
+      });
+      if (!existingChatId) {
+        const newChatId = chatIdRepository.create({ chatId });
+        await chatIdRepository.save(newChatId);
+      }
+    } catch (error) {
+      console.error("Error saving chatId:", error);
+    }
   });
 
 export const POST = webhookCallback(bot, 'std/http')

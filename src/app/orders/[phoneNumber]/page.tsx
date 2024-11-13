ed9bedbcd96dfd57acceab784f8f2a398d14d59c
Valery Piniazik <val.piniazik@gmail.com>
2024-11-13 21:20:26 +0200

    extracted phone number

--- src/app/orders/[phoneNumber]/page.tsx
+++ src/app/orders/[phoneNumber]/page.tsx
@@ -1,5 +1,9 @@
 "use client";
+import { useParams } from "next/navigation";
 
 export default function OrdersPage() {
-  return "Orders Page";
+  const params = useParams();
+  const phoneNumber = params.phoneNumber;
+
+  return `Orders Page: ${phoneNumber}`;
 }
